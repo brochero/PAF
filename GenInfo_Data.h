67153a5ebbc3c5b6613e11319424157a0fa9f6e3
brochero <javier.brochero.cifuentes@cern.ch>
2015-02-27 21:54:32 +0100

    Temporal solution to define GEN branches in DATA samples

---
+++ GenInfo_Data.h
@@ -0,0 +1,44 @@
+//#ifdef __ISMC
+
+float T_Event_nTruePU;
+float T_METgen_ET;
+float T_METgen_Phi;
+
+
+
+
+vector<float> *T_JetAKCHS_GenJet_Px;
+vector<float> *T_JetAKCHS_GenJet_Py;
+vector<float> *T_JetAKCHS_GenJet_Pz;
+vector<float> *T_JetAKCHS_GenJet_Energy;
+vector<float> *T_Gen_ElecSt3_PID;
+vector<float> *T_Gen_MuonSt3_PID;
+vector<float> *T_Gen_TauSt3_PID;
+vector<float> *T_Gen_TauSt3_IsLepDec;
+vector<float> *T_Gen_TauSt3_LepDec_PID;
+vector<float> *T_Gen_MuonSt3_Px;
+vector<float> *T_Gen_MuonSt3_Py;
+vector<float> *T_Gen_MuonSt3_Pz;
+vector<float> *T_Gen_MuonSt3_Energy;
+vector<float> *T_Gen_ElecSt3_Px;
+vector<float> *T_Gen_ElecSt3_Py;
+vector<float> *T_Gen_ElecSt3_Pz;
+vector<float> *T_Gen_ElecSt3_Energy;
+vector<float> *T_Gen_TauSt3_LepDec_Px;
+vector<float> *T_Gen_TauSt3_LepDec_Py;
+vector<float> *T_Gen_TauSt3_LepDec_Pz;
+vector<float> *T_Gen_TauSt3_LepDec_Energy;
+
+vector<float> *T_Gen_tSt3_PID;
+vector<float> *T_Gen_tSt3_T_Gen_tSt3_Px;
+vector<float> *T_Gen_tSt3_T_Gen_tSt3_Py;
+vector<float> *T_Gen_tSt3_T_Gen_tSt3_Pz;
+vector<float> *T_Gen_tSt3_T_Gen_tSt3_Energy;
+
+vector<float> *T_Gen_tSt3_Px;
+vector<float> *T_Gen_tSt3_Py;
+vector<float> *T_Gen_tSt3_Pz;
+vector<float> *T_Gen_tSt3_Energy;
+
+
+//#endif
